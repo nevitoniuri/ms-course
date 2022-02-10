5ac8f12c7bf2fc8e5518ed0fec589de74014e24d
Iuri Neviton <iurineviton@gmail.com>
2022-02-10 14:05:08 -0300

    Container references

--- hr-worker/src/main/java/com/devsuperior/hrworker/entities/Worker.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/entities/Worker.java
@@ -13,65 +13,65 @@ import javax.persistence.Table;
 @Table(name = "tb_worker")
 public class Worker implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	private String name;
+    private String name;
 
-	private Double dailyIncome;
+    private Double dailyIncome;
 
-	public Worker() {
+    public Worker() {
 
-	}
+    }
 
-	public Worker(Long id, String name, Double dailyIncome) {
-		this.id = id;
-		this.name = name;
-		this.dailyIncome = dailyIncome;
-	}
+    public Worker(Long id, String name, Double dailyIncome) {
+        this.id = id;
+        this.name = name;
+        this.dailyIncome = dailyIncome;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public Double getDailyIncome() {
-		return dailyIncome;
-	}
+    public Double getDailyIncome() {
+        return dailyIncome;
+    }
 
-	public void setDailyIncome(Double dailyIncome) {
-		this.dailyIncome = dailyIncome;
-	}
+    public void setDailyIncome(Double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Worker other = (Worker) obj;
-		return Objects.equals(id, other.id);
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Worker other = (Worker) obj;
+        return Objects.equals(id, other.id);
+    }
 
 }
